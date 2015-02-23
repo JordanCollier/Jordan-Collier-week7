22227a954b9be9bfa39fdf456c2cd0c4456b9b69
Jordan Collier <jordanicollier@gmail.com>
2015-02-23 14:38:21 -0700

    write time of day method for course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,12 @@ class Course < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true
 
+  def time_of_day
+     if self.tod == true
+       "day"
+    else
+       "night"
+    end
+  end
 
 end
