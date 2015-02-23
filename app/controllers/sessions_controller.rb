2dd16d16ba7383c321372a1436855a8016a2a1ac
Jordan Collier <jordanicollier@gmail.com>
2015-02-23 11:34:48 -0700

    style view and fix routing in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(user_name: params[:user_name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Week 7 and you are still here!"
+      redirect_to users_path, notice: "Week 7 and you are still here!"
     else
       flash[:login_alert] = "Username / password combination is invalid, stop trying to hack"
       redirect_to login_path
